135eeee8762abffb898d453776135b0486a47c36
nathaliabruno <nathi.bruno@gmail.coml>
2018-09-23 03:11:54 -0300

    MTA | Add active page

--- app/index.js
+++ app/index.js
@@ -9,29 +9,18 @@ import 'styles/index.sass';
 // START YOUR APP HERE
 // ================================
 
-import getCharacters from './character'
-import {requestPage} from './pagination'
+import {requestPage, listenerPagination} from './pagination'
 import search from './search'
 
 function init() {
+    const input = document.getElementById('input-search')
 
-    const pagesItems = document.getElementsByClassName('content-pagination-list-item')
-
-    for (var i = 0; i < pagesItems.length; i++) {
-
-        pagesItems[i].firstChild.addEventListener('click', e => {
-            e.preventDefault
-            let _this = e.target
-            history.pushState('', _this.href)
-        })
-    }
+    requestPage()
 
     window.addEventListener('popstate', requestPage);
 
-    getCharacters(`https://gateway.marvel.com/v1/public/characters?apikey=f804a6ba72e8f9e0aa1f02098a4d9760&limit=10&hash=798cc55b71bd99cdbb17ea46e4d9ecc4&ts=1`)
 
-    const input = document.getElementById('input-search')
-
+    listenerPagination()
     input.addEventListener('keyup', (e) => search(e))
 
 }
--- app/pagination.js
+++ app/pagination.js
@@ -23,19 +23,67 @@ export default function mountsPaginationHTML(pagination) {
             ul.appendChild(li)
 
         })
-
         nav.appendChild(ul)
+        setActivePage()
+    }
+}
+
+export function listenerPagination() {
+    const pagesItems = document.getElementsByClassName('content-pagination-list-item')
+
+    for (var i = 0; i < pagesItems.length; i++) {
+        pagesItems[i].firstChild.addEventListener('click', e => {
+            e.preventDefault
+            let _this = e.target
+            history.pushState('', _this.href)
+        })
     }
 }
 
 export function requestPage() {
     let offset = 0
     let pageNumber = window.location.hash
-    pageNumber = pageNumber.replace('#', '')
 
-    let linkPage = document.getElementById(`page_${pageNumber}`)
-    offset = linkPage.dataset.offset
+    if(pageNumber) {
 
-    getCharacters(`https://gateway.marvel.com/v1/public/characters?apikey=f804a6ba72e8f9e0aa1f02098a4d9760&limit=10&hash=798cc55b71bd99cdbb17ea46e4d9ecc4&ts=1&offset=${offset}`)
+        pageNumber = pageNumber.replace('#', '')
+
+        offset = (pageNumber - 1) * 10
+
+        getCharacters(`https://gateway.marvel.com/v1/public/characters?apikey=f804a6ba72e8f9e0aa1f02098a4d9760&limit=10&hash=798cc55b71bd99cdbb17ea46e4d9ecc4&ts=1&offset=${offset}`)
+    } else {
+        getCharacters(`https://gateway.marvel.com/v1/public/characters?apikey=f804a6ba72e8f9e0aa1f02098a4d9760&limit=10&hash=798cc55b71bd99cdbb17ea46e4d9ecc4&ts=1&offset=${offset}`)
+    }
 
 }
+
+
+function setActivePage() {
+    let pageNumber = window.location.hash
+    let linkPage = ''
+
+    if(pageNumber) {
+
+        pageNumber = pageNumber.replace('#', '')
+
+        linkPage = document.getElementById(`page_${pageNumber}`)
+
+    } else {
+        linkPage = document.getElementById('page_1')
+    }
+    if (linkPage) {
+        linkPage.classList.add('--active')
+        getPaginationWidth(linkPage)
+    }
+}
+
+function getPaginationWidth(element) {
+    const itemW = element.offsetWidth
+    const totalPages = document.getElementsByClassName('content-pagination-list-item').length
+    const ul = document.getElementById('pagination').firstChild
+    console.log(ul)
+    let width = ((itemW + 20) * totalPages) + 'px'
+
+     ul.style.width = width
+}
+
