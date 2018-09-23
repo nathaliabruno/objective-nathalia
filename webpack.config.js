9f2a6158561afb35aa0a5f99dbb2d4e9b8150f51
nathaliabruno <nathi.bruno@gmail.coml>
2018-09-23 16:40:57 -0300

    MTA | Config webpack to accept SVG images | Finish desktop details visual

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,7 @@ module.exports = {
 
             // IMAGES
             {
-                test: /\.(jpe?g|png|gif)$/,
+                test: /\.(jpe?g|png|gif|svg)$/,
                 loader: 'file-loader',
                 options: {
                     name: '[path][name].[ext]'
